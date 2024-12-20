6126c2fbc1b777dcaac95df4b09288200ee51748
Ali-Sid <alisiddiqui465@gmail.com>
2024-12-21 04:16:17 +0530

    add logo

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Send } from 'lucide-react';
+// import React from 'react';
+// import { Send } from 'lucide-react';
+import logo from "./assets/logo.png";
 
 export function Logo() {
   return (
     <div className="flex items-center space-x-2">
-      <Send className="h-8 w-8 text-green-600 transform -rotate-12" />
+      {/* <Send className="h-8 w-8 text-green-600 transform -rotate-12" />
       <span className="text-2xl font-bold bg-gradient-to-r from-green-600 to-green-400 bg-clip-text text-transparent">
         Bill Bhejo
-      </span>
+      </span> */}
+      <img src={logo} alt="Bill Bhejo" className="h-14 w-44" />
     </div>
   );
 }
