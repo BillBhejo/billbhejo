3591a2ee484bc90a13109def0db7110ebb2470b3
Ali-Sid <alisiddiqui465@gmail.com>
2025-01-11 16:25:31 +0530

    add useEffect and useLocation for smooth navigation

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,8 +4,20 @@ import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
 import Pricing from "./components/Pricing";
 import Footer from "./components/Footer";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.substring(1));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-green-50 to-white">
       <Header />
